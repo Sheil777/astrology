0dd8d6740a67271e39f998a8e1c1a9205198fbac
SheilAPK <karaulov.z@mail.ru>
2024-04-02 15:43:04 +0300

    Сделал клик на форму

--- template/js/scripts.js
+++ template/js/scripts.js
@@ -1,13 +1,21 @@
 const swiperForm = new Swiper('.form', {
   slidesPerView: 1,
   effect: "cube",
-  // effect: "flip",
-  // initialSlide: 1,
-});
+  allowTouchMove: false,
 
+});
 
 const swiperFeedback = new Swiper('.feedback__slider', {
   spaceBetween: 20,
   slidesPerView: 2.7,
-  
+});
+
+const butNext = document.querySelectorAll('.form__next');
+butNext.forEach(element => {
+  element.addEventListener('click', function(){
+    
+
+    // Смена слайда
+    swiperForm.slideNext();
+  });
 });
