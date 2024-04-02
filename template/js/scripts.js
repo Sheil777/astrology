a838cdd92934d597658913d62b3001b39d7db0d9
SheilAPK <karaulov.z@mail.ru>
2024-04-02 14:16:56 +0300

    Сверстана вторая страница формы

--- template/js/scripts.js
+++ template/js/scripts.js
@@ -1,4 +1,12 @@
-const swiper = new Swiper('.feedback__slider', {
+const swiperForm = new Swiper('.form', {
+  slidesPerView: 1,
+  effect: "cube",
+  // effect: "flip",
+  initialSlide: 1,
+});
+
+
+const swiperFeedback = new Swiper('.feedback__slider', {
   spaceBetween: 20,
   slidesPerView: 2.7,
   
